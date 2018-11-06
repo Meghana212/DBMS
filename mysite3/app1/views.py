7c13787cdd22b020d6910fdfa6fa5ac5d02b9d91
Meghana Peshwa <31281556+Meghana212@users.noreply.github.com>
2018-11-06 22:07:46 +0530

    Add files via upload

--- mysite3/app1/views.py
+++ mysite3/app1/views.py
@@ -6,7 +6,9 @@ from django.views.generic import ListView, CreateView, UpdateView
 from django.urls import reverse_lazy
 from .models import Airport,Passenger
 from django.db import connection
-pi = 1
+from django.template import Context, Template
+NOP = 1
+n=1
 # Create your views here.
 '''def index(request):
     data = Airport.objects.all()
@@ -20,10 +22,14 @@ def index(request):
     if request.method=='GET':
         data = Airport.objects.all()
         return render(request,'mysite3/index1.html',{'data':data})
-    if request.method=='POST':
+    elif request.method=='POST':
         cursor = connection.cursor()
         fro = request.POST.get('from')
         to = request.POST.get('to')
+        global NOP,n
+        NOP = request.POST.get('NOP')
+        NOP = int(NOP)
+        n=NOP
         cursor.execute("SELECT ap_id from airport where city=%s",[fro])
         s1 = cursor.fetchall()
         s = s1[0]
@@ -34,10 +40,14 @@ def index(request):
         #arrival = request.POST.get('arrival')
         depart = str(depart)
         depart = depart + '%'
-        cursor.execute("SELECT Class,Fare,Departure from fare f,flight_schedule fs,Route r where r.source_airport=%s and r.destination_airport = %s and r.R_ID=f.Route and f.Route = fs.R_ID and fs.Departure like %s",[s,d,depart])
-        avf = cursor.fetchall()
-        connection.close()
-        return render(request,'mysite3/details.html',{'avf':avf})
+        res_count=cursor.execute("SELECT Class,Fare,Departure from fare f,flight_schedule fs,Route r where r.source_airport=%s and r.destination_airport = %s and r.R_ID=f.Route and f.Route = fs.R_ID and fs.Departure like %s",[s,d,depart])
+        if res_count>0:
+            avf = cursor.fetchall()
+            connection.close()
+            return render(request,'mysite3/details.html',{'avf':avf})
+        else:
+            connection.close()
+            return render(request,'mysite3/details-no-flight.html')
     else:
         data = Airport.objects.all()
         return render(request,'mysite3/index1.html',{'data':data})
@@ -47,17 +57,20 @@ def index(request):
     return render(request,'mysite3/details.html')'''
 def forms(request):
     if request.method=='GET':
-        return render(request,'mysite3/forms.html')
+        return render(request,'mysite3/forms.html', {'NOP':range(1,NOP+1),'n':n})
     if request.method=='POST':
         fname = request.POST.get('first_name')
         mname = request.POST.get('middle_name')
         lname = request.POST.get('last_name')
         em = request.POST.get('EMAIL')
-        global pi
-        p1 = Passenger(p_id=pi,firstname=fname,middlename=mname,lastname=lname,email=em)
+        nationality = request.POST.get('nat')
+        age = request.POST.get('Age')
+        ph_no = request.POST.get('Phone_no.')
+        p1 = Passenger(firstname=fname,middlename=mname,lastname=lname,email=em,nationality=nationality,age=age,phone_no=ph_no)
+
         p1.save()
-        pi =pi+1
         return render(request,'mysite3/forms.html')
+
 def finalpage(request):
     return render(request,'mysite3/finalpage.html')
 
@@ -65,7 +78,7 @@ import MySQLdb
 
 #def list(request):
 #    db = MySQLdb.connect(user='root', db='A',  passwd='rizwan1998', host='localhost')
-#    cursor = db.cursor()wwwwwwwwwwww
+#    cursor = db.cursor()
 #    cursor.execute('SELECT * FROM Aircraft')
 #    names = [row[0] for row in cursor.fetchall()]
 #    db.close()i
